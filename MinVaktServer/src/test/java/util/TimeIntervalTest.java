b42d897a6f5b1111e75092b2e64c86909966d3e3
magnussh <magnus.s.hammer@gmail.com>
2017-01-10 15:09:50 +0100

    Added new test classes.

---
+++ MinVaktServer/src/test/java/util/TimeIntervalTest.java
@@ -0,0 +1,88 @@
+package util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by magnu on 10.01.2017.
+ */
+public class TimeIntervalTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getStart() throws Exception {
+
+    }
+
+    @Test
+    public void getEnd() throws Exception {
+
+    }
+
+    @Test
+    public void setDays() throws Exception {
+
+    }
+
+    @Test
+    public void getMinutes() throws Exception {
+
+    }
+
+    @Test
+    public void getDays() throws Exception {
+
+    }
+
+    @Test
+    public void addDay() throws Exception {
+
+    }
+
+    @Test
+    public void overlaps() throws Exception {
+
+    }
+
+    @Test
+    public void daysMatch() throws Exception {
+
+    }
+
+    @Test
+    public void overlapWith() throws Exception {
+
+    }
+
+    @Test
+    public void hasDays() throws Exception {
+
+    }
+
+    @Test
+    public void of() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
---
+++ MinVaktServer/src/test/java/util/TimeUtilTest.java
@@ -0,0 +1,33 @@
+package util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by magnu on 10.01.2017.
+ */
+public class TimeUtilTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void dayOfWeekOf() throws Exception {
+
+    }
+
+    @Test
+    public void shiftsOverlap() throws Exception {
+
+    }
+
+}
