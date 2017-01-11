734ae88b8d2879efd3ab01a8ded45f43b5c843f1
Faiter119 <olavh96@gmail.com>
2017-01-11 10:02:35 +0100

    Boot server now works maybe

---
+++ MinVaktServer/src/main/resources/static/server.js
@@ -0,0 +1,28 @@
+
+const react = require('react');
+const reactDOM = require('react-dom')
+const client = require('./client');
+const express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser');
+
+
+const app = express();
+
+document.ready(function () {
+
+    console.log("hi there");
+
+
+
+
+});
+/*app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '.', 'build', 'index.html'));
+});
+app.use(express.static(path.resolve(__dirname, '.', 'build')));
+var server = app.listen(8080, () => {
+    console.log('Kjører server på port 8080');
+});*/
+
+
