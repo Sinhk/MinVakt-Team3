96452a423942bdf6f9de71966c7ce1a22cc517b0
sindre <sindre.kroknes@gmail.com>
2017-01-11 09:08:24 +0100

    Modified annotations

--- MinVaktServer/src/main/java/minvakt/controller/UserController.java
+++ MinVaktServer/src/main/java/minvakt/controller/UserController.java
@@ -2,8 +2,9 @@ package minvakt.controller;
 
 import minvakt.datamodel.User;
 import minvakt.managers.UserManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,14 +19,14 @@ public class UserController {
         manager.addUser(new User("olavh96@gmail.com", 93240605, "Ostostost--", 100));
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public List<User> getUsers() {//@RequestParam(value="name", defaultValue="World") String name) {
 
         return manager.getUserList();
 
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public boolean addUser(User user) {
 
         return manager.addUser(user);
