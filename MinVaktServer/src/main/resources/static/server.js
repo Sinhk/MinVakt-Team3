a99f64e0d4b51b8fa53186cb26f36f0bc2b3e7ab
Faiter119 <olavh96@gmail.com>
2017-01-11 10:30:13 +0100

    minor changes

--- MinVaktServer/src/main/resources/static/server.js
+++ MinVaktServer/src/main/resources/static/server.js
@@ -17,12 +17,5 @@ document.ready(function () {
 
 
 });
-/*app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '.', 'build', 'index.html'));
-});
-app.use(express.static(path.resolve(__dirname, '.', 'build')));
-var server = app.listen(8080, () => {
-    console.log('Kjører server på port 8080');
-});*/
 
 
