c85281760a32b3955ba5769a27ecb1b6d55bd782
magnussh <magnussh@stud.ntnu.no>
2017-01-12 13:18:53 +0100

    Created test class for InfoValidator.

---
+++ MinVaktServer/src/test/java/minvakt/util/InfoValidatorTest.java
@@ -0,0 +1,21 @@
+package minvakt.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by magnu on 12.01.2017.
+ */
+public class InfoValidatorTest {
+    @Test
+    public void checkPasswordRequirements() throws Exception {
+
+    }
+
+    @Test
+    public void checkEmailRequirements() throws Exception {
+
+    }
+
+}
