ede441b9767b77824614c75a62367ac82c4dfc09
magnussh <magnussh@stud.ntnu.no>
2017-01-12 11:40:06 +0100

    Updated ShiftRepo.

--- MinVaktServer/src/main/java/minvakt/repos/ShiftRepository.java
+++ MinVaktServer/src/main/java/minvakt/repos/ShiftRepository.java
@@ -4,11 +4,8 @@ import minvakt.datamodel.Shift;
 import minvakt.datamodel.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ShiftRepository extends CrudRepository<User, Integer> {
+public interface ShiftRepository extends CrudRepository<Shift, Integer> {
 
     //Page<UserEntity> findAll(Pageable pageable);
 
-    Shift findBy(String name);
-
-    Shift findByEmail(String email);
 }
