c7700c848a97ec01b44c513fe71fcb543383e866
magnussh <magnussh@stud.ntnu.no>
2017-01-12 13:27:32 +0100

    Created new JUnit test class.

---
+++ MinVaktServer/src/test/java/minvakt/util/RandomStringTest.java
@@ -0,0 +1,16 @@
+package minvakt.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by magnu on 12.01.2017.
+ */
+public class RandomStringTest {
+    @Test
+    public void nextString() throws Exception {
+
+    }
+
+}
