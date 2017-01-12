2d994c73cb4fed911cd7f611a16748e80d7e2511
magnussh <magnussh@stud.ntnu.no>
2017-01-12 13:38:50 +0100

    Added test for email checker.

--- MinVaktServer/src/test/java/minvakt/util/InfoValidatorTest.java
+++ MinVaktServer/src/test/java/minvakt/util/InfoValidatorTest.java
@@ -11,17 +11,26 @@ public class InfoValidatorTest {
     @Test
     public void checkPasswordRequirements() throws Exception {
         assertEquals(false, InfoValidator.checkPasswordRequirements("Hei"));
-        assertEquals(true, InfoValidator.checkPasswordRequirements("HHei1!!!"));
-        assertEquals(true, InfoValidator.checkPasswordRequirements("Hei123!!!!!! OST"));
         assertEquals(false, InfoValidator.checkPasswordRequirements("?"));
         assertEquals(false, InfoValidator.checkPasswordRequirements("\0"));
         assertEquals(false, InfoValidator.checkPasswordRequirements(null));
         assertEquals(false, InfoValidator.checkPasswordRequirements("!?!?!?!?"));
+
+        assertEquals(true, InfoValidator.checkPasswordRequirements("HHei1!!!"));
+        assertEquals(true, InfoValidator.checkPasswordRequirements("Hei123!!!!!! OST"));
     }
 
     @Test
     public void checkEmailRequirements() throws Exception {
+        assertEquals(false, InfoValidator.checkEmailRequirements("email?"));
+        assertEquals(false, InfoValidator.checkEmailRequirements("per-at-ost.no"));
+        assertEquals(false, InfoValidator.checkEmailRequirements("norge krøllalfa norge dått no"));
 
+        assertEquals(true, InfoValidator.checkEmailRequirements("abc@xyz.com"));
+        assertEquals(true, InfoValidator.checkEmailRequirements("bob@gmail.com"));
+        assertEquals(true, InfoValidator.checkEmailRequirements("sindre@nsa.gov"));
+        assertEquals(true, InfoValidator.checkEmailRequirements("kent@tech.as"));
+        assertEquals(true, InfoValidator.checkEmailRequirements("artyom@fsb.ru"));
     }
 
 }
