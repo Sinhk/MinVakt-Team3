fa35ca5a70cd70ba0bd3d6724940029d1f4f5e15
magnussh <magnussh@stud.ntnu.no>
2017-01-13 15:36:29 +0100

    Moved properties of Users_Shifts table to Shift, maybe fix for Hibernate nightmare?

--- MinVaktServer/Database Script.sql
+++ MinVaktServer/Database Script.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Shifts;
 DROP TABLE IF EXISTS Users_Shifts;
+DROP TABLE IF EXISTS Shift_type;
 DROP TABLE IF EXISTS Type;
 DROP TABLE IF EXISTS Department;
 DROP TABLE IF EXISTS Shift_Department;
@@ -13,8 +14,7 @@ CREATE TABLE IF NOT EXISTS Users (
     email VARCHAR(50) NOT NULL,
     user_type_id INT NOT NULL,
     position_percentage INT NOT NULL,
-    salt VARCHAR(100) NOT NULL,
-    hash VARCHAR(100) NOT NULL,
+    password BINARY(60) NOT NULL,
     CONSTRAINT user_pk PRIMARY KEY (user_id)
 );
 
@@ -22,14 +22,14 @@ CREATE TABLE IF NOT EXISTS Shifts (
     shift_id INT NOT NULL AUTO_INCREMENT,
     from_time DATETIME NOT NULL,
     to_time DATETIME NOT NULL,
+    shift_type_id INT NOT NULL,
+    responsible_id INT NOT NULL,
     CONSTRAINT shift_pk PRIMARY KEY (shift_id)
 );
 
 CREATE TABLE IF NOT EXISTS Users_Shifts (
     user_id INT NOT NULL,
     shift_id INT NOT NULL,
-    responsible BOOL NOT NULL,
-    shift_type_id INT NOT NULL,
     CONSTRAINT user_shift_pk PRIMARY KEY (user_id, shift_id)
 );
 
@@ -73,5 +73,8 @@ ALTER TABLE Shift_Department
 ALTER TABLE Shift_Department
     ADD CONSTRAINT shift_department_fk2 FOREIGN KEY(shift_id) REFERENCES Shifts(shift_id);
 
-ALTER TABLE Users_Shifts
-    ADD CONSTRAINT user_shift_fk FOREIGN KEY (shift_type_id) REFERENCES Shift_type(shift_type_id);
+ALTER TABLE Shifts
+    ADD CONSTRAINT shift_fk1 FOREIGN KEY (shift_type_id) REFERENCES Shift_type(shift_type_id);
+
+ALTER TABLE Shifts
+    ADD CONSTRAINT shift_fk2 FOREIGN KEY (responsible_id) REFERENCES Users(user_id);
