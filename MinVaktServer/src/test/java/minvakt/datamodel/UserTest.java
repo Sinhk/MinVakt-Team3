593530c2de61e6a6658e90f206ced6437e6e45f9
magnussh <magnussh@stud.ntnu.no>
2017-01-13 09:44:16 +0100

    Updated UserTest for new methods.

--- MinVaktServer/src/test/java/minvakt/datamodel/UserTest.java
+++ MinVaktServer/src/test/java/minvakt/datamodel/UserTest.java
@@ -12,10 +12,10 @@ import static org.mockito.Mockito.mock;
  * Created by magnu on 09.01.2017.
  */
 public class UserTest {
-    UserManager userManager;
+
     @Before
     public void setUp() throws Exception {
-        userManager = mock(UserManager.class);
+
     }
 
     @After
@@ -24,9 +24,87 @@ public class UserTest {
     }
 
     @Test
+    public void getUserId() throws Exception {
+
+    }
+
+    @Test
+    public void getFirstName() throws Exception {
+
+    }
+
+    @Test
+    public void getLastName() throws Exception {
+
+    }
+
+    @Test
+    public void getPositionPercentage() throws Exception {
+
+    }
+
+    @Test
+    public void getPhone() throws Exception {
+
+    }
+
+    @Test
+    public void setUserId() throws Exception {
+
+    }
+
+    @Test
+    public void setFirstName() throws Exception {
+
+    }
+
+    @Test
+    public void setLastName() throws Exception {
+
+    }
+
+    @Test
+    public void setEmail() throws Exception {
+
+    }
+
+    @Test
+    public void setPhone() throws Exception {
+
+    }
+
+    @Test
+    public void setEmployeeType() throws Exception {
+
+    }
+
+    @Test
+    public void setPositionPercentage() throws Exception {
+
+    }
+
+    @Test
+    public void setPassword() throws Exception {
+
+    }
+
+    @Test
+    public void getShiftsForUser() throws Exception {
+
+    }
+
+    @Test
+    public void addShiftToUser() throws Exception {
+
+    }
+
+    @Test
+    public void removeShift() throws Exception {
+
+    }
+
+    @Test
     public void addUserTest(){
-
-
 
     }
 
