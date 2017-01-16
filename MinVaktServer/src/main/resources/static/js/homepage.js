b52647e22c1fd04bc7af0dd67b0584cb701626d2
Stine <stinesoien@hotmail.com>
2017-01-16 14:03:32 +0100

    homepage done

--- MinVaktServer/src/main/resources/static/js/homepage.js
+++ MinVaktServer/src/main/resources/static/js/homepage.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-    $('#calendar').fullCalendar({
-        header: {
-            left:'prev',
-            center:'title',
-            right:'next'
-        },
-        columnFormat: 'dd',
-        eventLimit: 2,
-        eventClick: function(calEvent, jsEvent, view) {
-            $(this).html('<img src="'+calEvent.image+'" />');
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200, // Transition out duration
+            starting_top: '4%', // Starting top style attribute
+            ending_top: '10%', // Ending top style attribute
+            ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+                alert("Ready");
+                console.log(modal, trigger);
+            },
+            complete: function() { alert('Closed'); } // Callback for Modal close
         }
-    });
-    $('#calendar').fullCalendar('renderEvent', { title: 'Test Event 1', start: '2016-11-05', end: '2016-11-09', image: 'https://s-media-cache-ak0.pinimg.com/236x/38/cb/d0/38cbd0f6b73df4414c6831806b65a070.jpg' }, true);
-    $('#calendar').fullCalendar('renderEvent', { title: 'Test Event 2', start: '2016-11-07', end: '2016-11-12', image: 'https://s-media-cache-ak0.pinimg.com/236x/38/cb/d0/38cbd0f6b73df4414c6831806b65a070.jpg' }, true);
-    $('#calendar').fullCalendar('renderEvent', { title: 'Test Event 3', start: '2016-11-09', image: 'https://s-media-cache-ak0.pinimg.com/236x/38/cb/d0/38cbd0f6b73df4414c6831806b65a070.jpg' }, true);
+    );
 });
