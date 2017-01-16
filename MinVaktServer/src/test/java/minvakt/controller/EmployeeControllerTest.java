f4326da4b4dbe7ae656c240d7113f501069fd20a
magnussh <magnussh@stud.ntnu.no>
2017-01-16 15:12:23 +0100

    Created EmployeeControllerTest.java.

---
+++ MinVaktServer/src/test/java/minvakt/controller/EmployeeControllerTest.java
@@ -0,0 +1,76 @@
+package minvakt.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by magnu on 16.01.2017.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class EmployeeControllerTest {
+
+    @Mock
+    private EmployeeController mockedEmpCont;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getUsers() throws Exception {
+
+    }
+
+    @Test
+    public void addEmployee() throws Exception {
+
+    }
+
+    @Test
+    public void removeEmployee() throws Exception {
+
+    }
+
+    @Test
+    public void findUser() throws Exception {
+
+    }
+
+    @Test
+    public void changePasswordForUser() throws Exception {
+
+    }
+
+    @Test
+    public void getShiftsForUser() throws Exception {
+
+    }
+
+    @Test
+    public void getShiftsForUserInRange() throws Exception {
+
+    }
+
+    @Test
+    public void addShiftToUser() throws Exception {
+
+    }
+
+    @Test
+    public void removeShiftFromUser() throws Exception {
+
+    }
+
+}
