b849a06d88ff68e553d955fed633579e380d9c91
sindre <sindre.kroknes@gmail.com>
2017-01-16 12:38:53 +0100

    Third times the charm

--- MinVaktServer/src/main/java/minvakt/config/SpringWebConfig.java
+++ MinVaktServer/src/main/java/minvakt/config/SpringWebConfig.java
@@ -1,6 +1,7 @@
 package minvakt.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -53,7 +54,9 @@ public class SpringWebConfig
                         .registerModule(new Hibernate5Module())
                         .registerModule(new ParameterNamesModule())
                         .registerModule(new Jdk8Module())
-                        .registerModule(new JavaTimeModule());
+                        .registerModule(new JavaTimeModule())
+                        //.setDateFormat()
+                        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             }
         }
     }
