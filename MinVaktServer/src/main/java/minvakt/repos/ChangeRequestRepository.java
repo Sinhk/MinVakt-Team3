a52659b07ef282c5385f05eac2db3bf406a960c6
magnussh <magnussh@stud.ntnu.no>
2017-01-18 13:45:16 +0100

    Added ChangeRequest class and repo.

---
+++ MinVaktServer/src/main/java/minvakt/repos/ChangeRequestRepository.java
@@ -0,0 +1,12 @@
+package minvakt.repos;
+
+import minvakt.datamodel.ChangeRequest;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by magnu on 18.01.2017.
+ */
+public interface ChangeRequestRepository extends CrudRepository<ChangeRequest, Integer> {
+
+
+}
