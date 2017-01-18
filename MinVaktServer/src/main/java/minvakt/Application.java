12bcb47b0cec47a3e2b577c2a54fed69e83422db
sindre <sindre.kroknes@gmail.com>
2017-01-18 17:36:57 +0100

    remove repo automapping

--- MinVaktServer/src/main/java/minvakt/Application.java
+++ MinVaktServer/src/main/java/minvakt/Application.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
+@SpringBootApplication(exclude = {ThymeleafAutoConfiguration.class, RepositoryRestMvcAutoConfiguration.class})
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
