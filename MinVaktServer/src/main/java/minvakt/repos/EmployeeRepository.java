93b1229e29ea82cd697fca219d190dc2ad43d92c
Sindre Kroknes <sindre.kroknes@gmail.com>
2017-01-19 02:51:20 +0100

    optimizing js

--- MinVaktServer/src/main/java/minvakt/repos/EmployeeRepository.java
+++ MinVaktServer/src/main/java/minvakt/repos/EmployeeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
@@ -21,6 +22,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     @Query("SELECT e FROM Employee e LEFT JOIN Shift s ON e.employeeId = s.responsibleEmployeeId WHERE s.shiftId = ?1")
     Employee findResponsibleForShift(int shift_id);
+
+    Optional<Employee> findByShiftAssignments_ShiftAndShiftAssignments_ResponsibleTrue(Shift shift);
 
     //void setPassword(User user, String password);
 
