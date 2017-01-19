839d2d664644df1aa1a5b5bcd78b841b791b5c56
Sindre Kroknes <sindre.kroknes@gmail.com>
2017-01-19 02:50:39 +0100

    department

---
+++ MinVaktServer/src/main/java/minvakt/datamodel/Department.java
@@ -0,0 +1,56 @@
+package minvakt.datamodel;
+
+import javax.persistence.*;
+
+/**
+ * MinVakt-Team3
+ * 15.01.2017
+ */
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id", nullable = false)
+    private Integer departmentId;
+
+    @Basic
+    @Column(name = "department_name", nullable = false, length = 30)
+    private String departmentName;
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Department that = (Department) o;
+
+        if (departmentId != null ? !departmentId.equals(that.departmentId) : that.departmentId != null) return false;
+        if (departmentName != null ? !departmentName.equals(that.departmentName) : that.departmentName != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = departmentId != null ? departmentId.hashCode() : 0;
+        result = 31 * result + (departmentName != null ? departmentName.hashCode() : 0);
+        return result;
+    }
+}
