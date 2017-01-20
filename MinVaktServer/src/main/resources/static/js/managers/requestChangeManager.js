d245ea63982c4f364308c147849a0395219a0d88
Faiter119 <olavh96@gmail.com>
2017-01-20 10:06:18 +0100

    js managers & cleanup

---
+++ MinVaktServer/src/main/resources/static/js/managers/requestChangeManager.js
@@ -0,0 +1,23 @@
+/**
+ * Created by OlavH on 20-Jan-17.
+ */
+
+function requestChangeForShift(shift_id, user1_id, user2_id) {
+
+    // TODO ny controller
+    $.ajax({
+        url: "/requestchange/",
+        type: "POST",
+        contentType: "Application/JSON",
+        data: jsonUser,
+        data2: category_id,
+        success: function (data) {
+
+            callback(data);
+        },
+        error: function (data) {
+            console.log("Error: " + data);
+        }
+    });
+
+}
