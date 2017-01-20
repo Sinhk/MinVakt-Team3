e3eaf10c4fb5c3ccab2ae2e74fcec6e6e622f137
elisabethmesoy <elisabeth-nm@hotmail.com>
2017-01-20 09:32:42 +0100

    fikset på absence

--- MinVaktServer/src/main/resources/static/js/absence.js
+++ MinVaktServer/src/main/resources/static/js/absence.js
@@ -22,4 +22,14 @@ $(document).ready(function(){
 
     });
 
+    $("#table tr").click(function(){
+        $(this).addClass('selected').siblings().removeClass('selected');
+        var value=$(this).find('td:first').html();
+        alert(value);
+    });
+
+    $('.ok').on('click', function(e){
+        alert($("#table tr.selected td:first").html());
+    });
+
 });
