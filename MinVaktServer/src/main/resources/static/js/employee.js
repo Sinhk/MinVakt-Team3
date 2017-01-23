ab1bd13516cf30162f43ccb843c29490ce5eedd0
elisabethmesoy <elisabeth-nm@hotmail.com>
2017-01-23 09:07:27 +0100

    fikset på absence

--- MinVaktServer/src/main/resources/static/js/employee.js
+++ MinVaktServer/src/main/resources/static/js/employee.js
@@ -32,17 +32,23 @@ $(document).ready(function(){
 
     $("#myTable").tablesorter();
     $("#myTable").tablesorter( {sortList: [[0,0], [1,0]]} );
-    /*
-    $(".modal-trigger").click(function(e){
-        e.preventDefault();
-        dataModal = $(this).attr("data-modal");
-        $("#" + dataModal).css({"display":"block"});
-    });
 
-    $(".close-modal, .modal-sandbox").click(function(){
-        $(".modal").css({"display":"none"});
-    });
-*/
+    function myFunction() {
+        var input, filter, table, tr, td, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
 
-
+        for(i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if(td) {
+                if(td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
 });
