2221c6b16524a5389d7144c40e2b5c2b72a98335
Sindre Kroknes <sindre.kroknes@gmail.com>
2017-01-23 20:57:37 +0100

    fixed changePassword backend. Now it actually validates old password

--- MinVaktServer/src/main/java/minvakt/config/SecurityConfig.java
+++ MinVaktServer/src/main/java/minvakt/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import minvakt.managers.AUserDetailsManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -91,5 +93,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- MinVaktServer/src/main/java/minvakt/managers/AUserDetailsManager.java
+++ MinVaktServer/src/main/java/minvakt/managers/AUserDetailsManager.java
@@ -1,5 +1,11 @@
 package minvakt.managers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,12 +17,15 @@ import org.springframework.util.Assert;
  * in project: MinVakt-Team3
  */
 public class AUserDetailsManager extends JdbcUserDetailsManager {
+    @Autowired
+    private AuthenticationManager authenticationManager;
     String usersByUsernameQuery = "select email,passwd,enabled from employee where email=?";
     String authoritiesByUsernameQuery = "SELECT email, IF(admin,'ADMIN','USER') from employee e Left JOIN employee_category ec ON e.category_id = ec.category_id WHERE email =?;";
     String changePasswordSql = "UPDATE employee SET passwd = ? WHERE email = ?";
     String createUserSql = "INSERT INTO employee (email,passwd,enabled) VALUES (?,?,?)";
     String updateUserSql = "UPDATE employee SET passwd = ?, enabled = ? WHERE email = ?";
     String userExistsSql = "SELECT email FROM employee WHERE email= ?";
+
 
 
     @Override
@@ -42,7 +51,33 @@ public class AUserDetailsManager extends JdbcUserDetailsManager {
     @Override
     public void changePassword(String oldPassword, String newPassword) {
         newPassword = encrypt(newPassword);
-        super.changePassword(oldPassword, newPassword);
+        Authentication currentUser = SecurityContextHolder.getContext()
+                .getAuthentication();
+
+        if (currentUser == null) {
+            // This would indicate bad coding somewhere
+            throw new AccessDeniedException(
+                    "Can't change password as no Authentication object found in context "
+                            + "for current user.");
+        }
+
+        String username = currentUser.getName();
+
+        // If an authentication manager has been set, re-authenticate the user with the
+        // supplied password.
+        if (authenticationManager != null) {
+            logger.debug("Reauthenticating user '" + username
+                    + "' for password change request.");
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    username, oldPassword));
+        } else {
+            logger.debug("No authentication manager set. Password won't be re-checked.");
+        }
+
+        logger.debug("Changing password for user '" + username + "'");
+
+        getJdbcTemplate().update(changePasswordSql, newPassword, username);
     }
 
     private String encrypt(String toEncrypt) {
