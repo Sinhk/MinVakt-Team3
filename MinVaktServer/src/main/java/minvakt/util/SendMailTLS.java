55b740aed40ec4c68374a76803cf94cb93ccecf9
magnussh <magnussh@stud.ntnu.no>
2017-01-24 14:43:05 +0100

    "Optimized" SendMailTLS, removed redundant code.

--- MinVaktServer/src/main/java/minvakt/util/SendMailTLS.java
+++ MinVaktServer/src/main/java/minvakt/util/SendMailTLS.java
@@ -12,12 +12,10 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class SendMailTLS {
-
     Session session;
 
     public SendMailTLS() {
-        final String username = "kennyboy083@gmail.com";
-        final String password = "!\"#123qweasd";
+        final String username = "kennyboy083@gmail.com", password = "!\"#123qweasd";
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -32,128 +30,50 @@ public class SendMailTLS {
         });
     }
 
-/*
-    public static void main(String[] args) {
-
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        final String username = "kennyboy083@gmail.com";
-        final String password = "!\"#123qweasd";
-        Session session;
-        session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
+    private void sendMail(Message message) {
         try {
-
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("noreply@minvakt.123"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse("k.l.k08394@gmail.com"));
-            message.setSubject("Velkommen til MinVakt");
-            message.setText("Hei\n"+
-                    "Velkommen til MinVakt\n" +
-                    "Her er ditt passord: "+ password +
-                    "\nhttps://minvakt.herokuapp.com");
-
             Transport.send(message);
-
-            System.out.println("Done");
-
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
     }
-*/
+
+    private void buildMessage(Message m, String email, String subj, String text) {
+        try {
+            m.setFrom(new InternetAddress("noreply@minvakt.123"));
+            m.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(email));
+            m.setSubject(subj);
+            m.setText(text);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void sendPassword(String email, String password) {
-
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("noreply@minvakt.123"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(email));
-            message.setSubject("Minvakt passord");
-            message.setText("Hei\n" +
-                    "Her er ditt passord: " + password +
-                    "\nhttps://minvakt.herokuapp.com");
-
-            Transport.send(message);
-
-            System.out.println("Email sent with password");
-
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+        Message message = new MimeMessage(session);
+        String subject = "Minvakt passord", text = ("Hei\n" + "Her er ditt passord: " + password + "\n\nhttps://minvakt.herokuapp.com");
+        buildMessage(message, email, subject, text);
+        sendMail(message);
     }
 
-    public void sendFreeGuard(String link, String email) {
-        try {
-
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("noreply@minvakt.123"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(email));
-            message.setSubject("Minvakt har ny vakt ledig");
-            message.setText("Hei\n" +
-                    "Her er en link til shiftet: " + link);
-
-            Transport.send(message);
-
-            System.out.println("Email sent with the guard link");
-
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+    public void sendFreeShift(String link, String email) {
+        Message message = new MimeMessage(session);
+        String subject = "Minvakt har en ny vakt ledig", text = ("Hei\n" + "Her er en link til vakten: " + link);
+        buildMessage(message, email, subject, text);
+        sendMail(message);
     }
 
-    public void sendFreeGuardToGroup(String link, String[] eMails) {
+    public void sendFreeShiftToGroup(String link, String[] eMails) {
         for(String eMail:eMails) {
-            try {
-
-                Message message = new MimeMessage(session);
-                message.setFrom(new InternetAddress("noreply@minvakt.123"));
-                message.setRecipients(Message.RecipientType.TO,
-                        InternetAddress.parse(eMail));
-                message.setSubject("Minvakt har ny vakt ledig");
-                message.setText("Hei\n" +
-                        "Her er en link til shiftet: " + link);
-
-                Transport.send(message);
-
-                System.out.println("Email sent with the guard link");
-
-            } catch (MessagingException e) {
-                throw new RuntimeException(e);
-            }
+            sendFreeShift(link, eMail);
         }
     }
 
-    public void sendAnswearOnGuardChange(String email, String answear) {
-
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("noreply@minvakt.123"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(email));
-            message.setSubject("Minvakt shift bytte");
-            message.setText("Hei\n"+
-                    answear +
-                    "\nhttps://minvakt.herokuapp.com"
-            );
-
-            Transport.send(message);
-
-            System.out.println("Email sent with guard change answear");
-
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+    public void sendAnswerOnShiftChange(String email, String answer) {
+        Message message = new MimeMessage(session);
+        String subject = "Minvakt shift bytte", text = ("Hei\n" + answer + "\nhttps://minvakt.herokuapp.com");
+        buildMessage(message, email, subject, text);
+        sendMail(message);
     }
-
 }
