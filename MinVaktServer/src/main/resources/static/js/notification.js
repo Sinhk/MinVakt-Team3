9693f51b42d9ed764134d75f86f31e3531509885
elisabethmesoy <elisabeth-nm@hotmail.com>
2017-01-24 11:44:56 +0100

    fikset på notifications

--- MinVaktServer/src/main/resources/static/js/notification.js
+++ MinVaktServer/src/main/resources/static/js/notification.js
@@ -4,5 +4,25 @@
 
 $(document).ready(function(){
     $('.collapsible').collapsible();
+
+    $("#button1").click(function () {
+        swal("Vakten ble byttet", "", "success")
+        $('#collapse1').hide();
+    });
+
+    $("#button2").click(function () {
+        swal("Vakten ble byttet", "", "success")
+        $('#collapse2').hide();
+    });
+
+    $("#button3").click(function () {
+        swal("Vakten ble ikke byttet", "", "error");
+        $('#collapse1').hide();
+    })
+
+    $("#button4").click(function () {
+        swal("Vakten ble ikke byttet", "", "error");
+        $('#collapse2').hide();
+    })
 });
 
