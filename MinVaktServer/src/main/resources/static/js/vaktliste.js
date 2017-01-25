42fe4f6f5d277771cec82c0f788b29fd2490e65f
Stine <stinesoien@hotmail.com>
2017-01-25 12:56:44 +0100

    Vaktlisten er tilbake som før

--- MinVaktServer/src/main/resources/static/js/vaktliste.js
+++ MinVaktServer/src/main/resources/static/js/vaktliste.js
@@ -4,18 +4,27 @@ $(document).ready(function () { // document ready
         schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
         displayEventTime: false,
         locale: "no",
-        timezone: "UTC+1",
+        timezone: "UTC",
         selectable: true,
         resourceAreaWidth: 230,
         //editable: true,
         aspectRatio: 1.5,
         scrollTime: '00:00',
         header: {
-            left: 'prev',
+            left: 'prev, promptResource today',
             center: 'title',
             right: 'next'
         },
         firstDay: 1,
+        defaultView: 'customWeek',
+        views: {
+            customWeek: {
+                type: 'timeline',
+                duration: {weeks: 1},
+                slotDuration: {days: 1},
+                buttonText: 'Vaktliste'
+            }
+        },
         dayNamesShort: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'],
         monthNames: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
         weekNumberTitle: 'Uke',
@@ -43,6 +52,9 @@ $(document).ready(function () { // document ready
                 console.log(event);
 
             })
+
+
+
         },
 
         select: function (start, end, jsEvent, view) {
@@ -95,56 +107,56 @@ $(document).ready(function () { // document ready
 
             })
 
-    }
-});
+        }
+    });
 
     /*getAllAssignedShifts(function (assignedShifts) {
 
-        for (var i = 0; i < assignedShifts.length; i++) {
+     for (var i = 0; i < assignedShifts.length; i++) {
 
-            const nonAssignedShift = assignedShifts[i];
+     const nonAssignedShift = assignedShifts[i];
 
-            console.log(nonAssignedShift);
+     console.log(nonAssignedShift);
 
-            getUserById(nonAssignedShift.employeeId, function (user) {
+     getUserById(nonAssignedShift.employeeId, function (user) {
 
-                getShiftWithId(nonAssignedShift.shiftId, function (shift) {
+     getShiftWithId(nonAssignedShift.shiftId, function (shift) {
 
-                    console.log(user);
-                    console.log(shift);
+     console.log(user);
+     console.log(shift);
 
-                    var resource = {
-                        id: user.employeeId,
-                        title: user.firstName + " " + user.lastName,
-                    }
+     var resource = {
+     id: user.employeeId,
+     title: user.firstName + " " + user.lastName,
+     }
 
-                    $('#calendar').fullCalendar('addResource', resource)
-
-
-                    var event = {
-
-                        id: shift.shiftId,
-                        resourceId: user.employeeId,
-                        start: shift.fromTime.split("T")[0],
-                        end: shift.toTime.split("T")[0],
-                        title: shift.fromTime.split("T")[1].substr(0, 5) + " - " + shift.toTime.split("T")[1].substr(0, 5),
-
-                        stick: true
-
-                    }
-
-                    $('#calendar').fullCalendar('renderEvent', event, true);
+     $('#calendar').fullCalendar('addResource', resource)
 
 
+     var event = {
+
+     id: shift.shiftId,
+     resourceId: user.employeeId,
+     start: shift.fromTime.split("T")[0],
+     end: shift.toTime.split("T")[0],
+     title: shift.fromTime.split("T")[1].substr(0, 5) + " - " + shift.toTime.split("T")[1].substr(0, 5),
+
+     stick: true
+
+     }
+
+     $('#calendar').fullCalendar('renderEvent', event, true);
 
 
-                })
-            })
-        }
-    })*/
+
+
+     })
+     })
+     }
+     })*/
     /*getAllShifts(function (events) {
-        calendar.fullCalendar('addEventSource', listToFullCalendarEventList(events, calendar.fullCalendar('getResources')));
-    });*/
+     calendar.fullCalendar('addEventSource', listToFullCalendarEventList(events, calendar.fullCalendar('getResources')));
+     });*/
 });
 
 function getUsersAndCreateResourceList(callback) {
@@ -169,5 +181,4 @@ function getUsersAndCreateResourceList(callback) {
     })
 
 }
-
 
