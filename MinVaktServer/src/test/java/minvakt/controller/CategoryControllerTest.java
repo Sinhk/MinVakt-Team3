7b1a2346f2514c0aa938212cdcddab610ece1fd6
magnussh <magnussh@stud.ntnu.no>
2017-01-27 15:09:37 +0100

    CategoryController 100% coverage.

--- MinVaktServer/src/test/java/minvakt/controller/CategoryControllerTest.java
+++ MinVaktServer/src/test/java/minvakt/controller/CategoryControllerTest.java
@@ -1,22 +1,51 @@
 package minvakt.controller;
 
-import org.junit.Before;
+import minvakt.datamodel.tables.pojos.EmployeeCategory;
+import minvakt.repos.CategoryRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by magnu on 20.01.2017.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class CategoryControllerTest {
-    @Before
-    public void setUp() throws Exception {
+    @InjectMocks
+    private CategoryController categoryController;
 
-    }
+    @Mock
+    private CategoryRepository categoryRepository;
+
 
     @Test
     public void getCategories() throws Exception {
+        // Mock
+        EmployeeCategory empCat = mock(EmployeeCategory.class);
 
+        // Stub
+        when(categoryRepository.findAll()).thenReturn(Arrays.asList(empCat));
+
+        // Get list
+        List<EmployeeCategory> list = categoryController.getCategories();
+
+        // Assert
+        assertEquals(empCat, list.get(0));
+        assertEquals(1, list.size());
+
+
+        // Verify
+        verify(categoryRepository).findAll();
     }
 
 }
